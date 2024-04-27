d0f539115eaa68b6223f679b828e93d41c0f70f6
Oksana Banshchykova <88445174+oxica@users.noreply.github.com>
2024-04-27 12:20:50 +0200

    12

--- script.js
+++ script.js
@@ -1,19 +1,45 @@
 const counters = document.querySelectorAll('.counter');
+const experience = document.querySelector(".experience-section");
 
-counters.forEach(counter => {
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > experience.offsetHeight + 150) {
+    counters.forEach(counter => {
     counter.innerText = '0';
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target');
         const c = +counter.innerText;
 
-        const increment = target / 200;
+        const increment = target / 20;
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`;
-            setTimeout(updateCounter, 30);
+            setTimeout(updateCounter, 300);
         } else {
             counter.innerText = target;
         }
     }
     updateCounter();
 })
+  } 
+}
+
+// counters.forEach(counter => {
+//     counter.innerText = '0';
+
+//     const updateCounter = () => {
+//         const target = +counter.getAttribute('data-target');
+//         const c = +counter.innerText;
+
+//         const increment = target / 200;
+//         if (c < target) {
+//             counter.innerText = `${Math.ceil(c + increment)}`;
+//             setTimeout(updateCounter, 30);
+//         } else {
+//             counter.innerText = target;
+//         }
+//     }
+//     updateCounter();
+// })
+
